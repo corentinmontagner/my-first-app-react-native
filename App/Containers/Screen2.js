834aaa82538d46d0a9e34b0955b227ab29213bda
Corentin Montagner <corentin.montagner@ultimadisplays.eu>
2021-02-03 09:17:55 +0100

    First commit

---
+++ App/Containers/Screen2.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function Screen2() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Screen2</Text>
+      </View>
+    );
+  }
+
+export default Screen2
